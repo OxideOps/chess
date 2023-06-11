9321b42521076ae66120ef5bb0729ea7a4412048
Dillon-Roller <dillonroller.18@gmail.com>
2023-06-11 18:48:06 -0400

    pieces implemented

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,9 +1,10 @@
-pub trait Piece {
+pub trait Piece: {
     fn color(&self) -> Color;
     fn can_move(&self, from: (usize, usize), to: (usize, usize)) -> bool;
     // Add other common methods here
 }
 
+#[derive(Copy, Clone)]
 pub struct Pawn {
     color: Color,
     has_moved: bool,
@@ -15,24 +16,8 @@ impl Piece for Pawn {
     }
 
     fn can_move(&self, from: (usize, usize), to: (usize, usize)) -> bool {
-        // Implement pawn-specific move logic
-        // Check if the move is valid for a pawn
-        // Return true or false accordingly
+        self.has_moved
     }
-
-    // Implement other methods specific to the pawn
-}
-
-// Implement other chess piece types similarly (e.g., Rook, Knight, Bishop, etc.)
-
-pub struct Board {
-    // Define the chess board and other board-related fields
-    // ...
-}
-
-impl Board {
-    // Implement board-related methods, such as move_piece, get_piece_at, etc.
-    // ...
 }
 
 #[derive(Clone, Copy)]
