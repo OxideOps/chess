f80a5ea7083c151219b4791ed61001970d7fbbf0
Dillon-Roller <dillonroller.18@gmail.com>
2023-06-12 21:04:24 -0400

    clean stuff up

--- src/board.rs
+++ src/board.rs
@@ -1,32 +1,88 @@
-use crate::pieces::Piece;
+use crate::pieces::{Piece, Position, Player};
 use crate::game::ChessError;
 
+const BOARD_SIZE: usize = 8;
+
 pub struct Board {
-    squares: [[Option<Box<dyn Piece>>; 8]; 8],
+    squares: [[Piece; 8]; 8],
 }
 
 impl Board {
     pub fn new() -> Self {
-        let squares: [[Option<Box<dyn Piece>>; 8]; 8] = Default::default();
+        let mut squares = [[Piece::None; BOARD_SIZE]; BOARD_SIZE];
+
+        // Initialize white pawns
+        for i in 0..8 {
+            squares[1][i] = Piece::Pawn(Player::White);
+        }
+
+        // Initialize black pawns
+        for i in 0..8 {
+            squares[6][i] = Piece::Pawn(Player::Black);
+        }
+
+        // Initialize the other white and black pieces
+
+        for i in 0..BOARD_SIZE {
+            squares[0]              = Board::get_back_rank(i, Player::White);
+            squares[BOARD_SIZE - 1] = Board::get_back_rank(i, Player::Black);
+        }
+
         Self { squares }
     }
 
-    pub fn get_piece_at(&self, position: (usize, usize)) -> Option<&dyn Piece> {
-        let (x, y) = position;
-        self.squares[x][y].as_deref()
+    fn get_back_rank(file: usize, player: Player) -> [Piece; 8] {
+        return [
+            Piece::Rook(player),
+            Piece::Knight(player),
+            Piece::Bishop(player),
+            Piece::Queen(player),
+            Piece::King(player),
+            Piece::Bishop(player),
+            Piece::Knight(player),
+            Piece::Rook(player),
+        ];
     }
 
-    pub fn move_piece(&mut self, from: (usize, usize), to: (usize, usize)) -> Result<(), ChessError> {
-        if let &Some(ref piece) = &self.squares[from.0][from.1] {
-            if piece.can_move(from, to) {
-                let piece = self.squares[from.0][from.1].take();
-                self.squares[to.0][to.1] = piece;
-                Ok(())
-            } else {
-                Err(ChessError::InvalidMove)
-            }
+    pub fn is_in_bounds(&self, position: Position) -> Result<(), ChessError> {
+        if position.x > 7 || position.y > 7 {
+            Err(ChessError::OutOfBounds)
         } else {
-            Err(ChessError::NoPieceAtPosition)
+            Ok(())
         }
     }
+
+    pub fn borrow_piece_at(&self, position: Position) -> Result<&Piece, ChessError> {
+        self.is_in_bounds(position)?;
+        Ok(&self.squares[position.x][position.y])
+    }
+
+    pub fn take_piece_at(&mut self, position: Position) -> Result<Piece, ChessError> {
+        self.is_in_bounds(position)?;
+        let piece = self.squares[position.x][position.y];
+        self.squares[position.x][position.y] = Piece::None;
+        Ok(piece)
+    }
+
+    pub fn set_piece_at(&mut self, position: Position, piece: Piece) -> Result<(), ChessError> {
+        self.is_in_bounds(position)?;
+        self.squares[position.x][position.y] = piece;
+
+        Ok(())
+    }
+
+    pub fn move_piece(&mut self, from: Position, to: Position) -> Result<(), ChessError> {
+        if self.borrow_piece_at(from)?.is_none() {
+            return Err(ChessError::NoPieceAtPosition);
+        }
+        
+        let piece = self.take_piece_at(from)?;
+
+        if piece.can_move(self, from, to)? {
+            self.set_piece_at(to, piece)?;
+        }
+        
+        Ok(())
+    }
 }
+
--- src/pieces.rs
+++ src/pieces.rs
@@ -1,28 +1,74 @@
-pub trait Piece: {
-    fn color(&self) -> Color;
-    fn can_move(&self, from: (usize, usize), to: (usize, usize)) -> bool;
-    // Add other common methods here
+use crate::{board::Board, game::ChessError};
+
+use std::mem;
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Piece {
+    Pawn(Player),
+    Knight(Player),
+    Bishop(Player),
+    Rook(Player),
+    Queen(Player),
+    King(Player),
+    None,
 }
 
-#[derive(Copy, Clone)]
-pub struct Pawn {
-    color: Color,
-    has_moved: bool,
+impl Piece {
+    pub fn is_none(&self) -> bool {
+        *self == Piece::None
+    }
+
+    pub fn is_some(&self) -> bool {
+        *self != Piece::None
+    }
+
+    pub fn take(&mut self) -> Piece {
+        mem::replace(self, Piece::None)
+    }
+    
+    pub fn can_move(&self, board: &mut Board, to: Position, from: Position) -> Result<bool, ChessError> {
+        let to_piece = board.borrow_piece_at(to)?;
+        let from_piece = board.borrow_piece_at(from)?;
+
+        //Do logic for each piece
+        match *self {
+            Piece::Pawn(..) => {
+                //Err(Piece::PawnError::SomeError)
+                return Ok(false)
+            }
+            Piece::Knight(..) => {
+                return Ok(false)
+            }
+            Piece::Bishop(..) => {
+                return Ok(false)
+            }
+            Piece::Rook(..) => {
+                //Err(Piece::RookError::SomeOtherError)
+                return Ok(true)
+            }
+            Piece::Queen(..) => {
+                return Ok(false)
+            }
+            Piece::King(..) => {
+                return Ok(false)
+            }
+            Piece::None => {
+                return Ok(false)
+            }
+        };
+        Ok(true)
+    }
 }
 
-impl Piece for Pawn {
-    fn color(&self) -> Color {
-        self.color
-    }
-
-    fn can_move(&self, from: (usize, usize), to: (usize, usize)) -> bool {
-        self.has_moved
-    }
+#[derive(Clone, Copy, PartialEq)]
+pub enum Player {
+    White,
+    Black
 }
 
 #[derive(Clone, Copy)]
-pub enum Color {
-    White,
-    Black
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
 }
 
