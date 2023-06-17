674ef57cb70592f539ac4eb9bde825579d96c0ed
Daniel Albl <daniel.albl@vivint.com>
2023-06-17 17:04:55 -0600

    Fix UI issues

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,8 @@ impl CustomWidget {
                 Player::Black => "black",
             };
             let background = match (position.x + position.y) % 2 {
-                0 => "Light",
-                1 => "Dark",
+                0 => "Dark",
+                1 => "Light",
                 _ => "",
             };
             Some("images/".to_owned() + player + name + background + ".png")
@@ -65,16 +65,15 @@ impl CustomWidget {
 
     fn draw_square(&self, ctx: &mut PaintCtx, position: Position) {
         if let Some(image_file) = self.get_image_file(position) {
-            // let image_data = get_image(image_file);
             let image_data = get_image(image_file);
             let image = ctx
                 .make_image(PIECE_SIZE, PIECE_SIZE, &image_data, ImageFormat::Rgb)
                 .unwrap();
-            let x0 = WINDOW_SIZE / 8.0 * (position.x as f64);
-            let y0 = WINDOW_SIZE / 8.0 * (position.y as f64);
+            let x0 = WINDOW_SIZE * position.x as f64 / 8.0;
+            let y1 = WINDOW_SIZE * (1.0 - (position.y as f64) / 8.0);
             ctx.draw_image(
                 &image,
-                Rect::new(x0, y0, x0 + WINDOW_SIZE / 8.0, y0 + WINDOW_SIZE / 8.0),
+                Rect::new(x0, y1 - WINDOW_SIZE / 8.0, x0 + WINDOW_SIZE / 8.0, y1),
                 InterpolationMode::Bilinear,
             );
         }
