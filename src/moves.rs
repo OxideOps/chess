38bf3f00bd1f8ec39770ccd7103371d92e76e94e
Dillon-Roller <dillonroller.18@gmail.com>
2023-06-16 20:24:28 -0400

    `get_pawn_advance_move()`

--- src/moves.rs
+++ src/moves.rs
@@ -8,71 +8,64 @@ pub struct Move {
 }
 
 impl Move {
-    const QUEEN_MOVES: [Move; 8] = [
-        Move { dx: 1, dy: 0 },
-        Move { dx: -1, dy: 0 },
-        Move { dx: 0, dy: 1 },
-        Move { dx: 0, dy: -1 },
-        Move { dx: 1, dy: 1 },
-        Move { dx: 1, dy: -1 },
-        Move { dx: -1, dy: 1 },
-        Move { dx: -1, dy: -1 },
+    const QUEEN_MOVES: [Self; 8] = [
+        Self { dx: 1, dy: 0 },
+        Self { dx: -1, dy: 0 },
+        Self { dx: 0, dy: 1 },
+        Self { dx: 0, dy: -1 },
+        Self { dx: 1, dy: 1 },
+        Self { dx: 1, dy: -1 },
+        Self { dx: -1, dy: 1 },
+        Self { dx: -1, dy: -1 },
     ];
-    const KNIGHT_MOVES: [Move; 8] = [
-        Move { dx: 1, dy: 2 },
-        Move { dx: 1, dy: -2 },
-        Move { dx: -1, dy: 2 },
-        Move { dx: -1, dy: -2 },
-        Move { dx: 2, dy: 1 },
-        Move { dx: 2, dy: -1 },
-        Move { dx: -2, dy: 1 },
-        Move { dx: -2, dy: -1 },
+    const KNIGHT_MOVES: [Self; 8] = [
+        Self { dx: 1, dy: 2 },
+        Self { dx: 1, dy: -2 },
+        Self { dx: -1, dy: 2 },
+        Self { dx: -1, dy: -2 },
+        Self { dx: 2, dy: 1 },
+        Self { dx: 2, dy: -1 },
+        Self { dx: -2, dy: 1 },
+        Self { dx: -2, dy: -1 },
     ];
 
-    pub fn get_pawn_advance_moves(player: Player, first_move: bool) -> &'static [Move] {
+    pub fn get_pawn_advance_move(player: Player) -> Self {
+        let dx;
+        let dy;
+
         match player {
-            Player::White => {
-                if first_move {
-                    &[Move { dx: 0, dy: -1 }, Move { dx: 0, dy: -2 }]
-                } else {
-                    &Self::QUEEN_MOVES[3..4]
-                }
-            }
-            Player::Black => {
-                if first_move {
-                    &[Move { dx: 0, dy: 1 }, Move { dx: 0, dy: 2 }]
-                } else {
-                    &Self::QUEEN_MOVES[2..3]
-                }
-            }
+            Player::White => (dx, dy) = (0, -1),
+            Player::Black => (dx, dy) = (0, 1),
         }
+
+        Self { dx, dy }
     }
 
-    pub fn get_pawn_capture_moves_white() -> &'static [Move] {
-        &[Move { dx: -1, dy: -1 }, Move { dx: 1, dy: -1 }]
+    pub fn get_pawn_capture_moves_white() -> &'static [Self] {
+        &[Self { dx: -1, dy: -1 }, Self { dx: 1, dy: -1 }]
     }
 
-    pub fn get_pawn_capture_moves_black() -> &'static [Move] {
-        &[Move { dx: 1, dy: 1 }, Move { dx: -1, dy: 1 }]
+    pub fn get_pawn_capture_moves_black() -> &'static [Self] {
+        &[Self { dx: 1, dy: 1 }, Self { dx: -1, dy: 1 }]
     }
 
-    pub fn get_queen_moves() -> &'static [Move] {
+    pub fn get_queen_moves() -> &'static [Self] {
         &Self::QUEEN_MOVES
     }
 
-    pub fn get_king_moves() -> &'static [Move] {
+    pub fn get_king_moves() -> &'static [Self] {
         &Self::QUEEN_MOVES
     }
 
-    pub fn get_rook_moves() -> &'static [Move] {
+    pub fn get_rook_moves() -> &'static [Self] {
         &Self::QUEEN_MOVES[0..4]
     }
 
-    pub fn get_bishop_moves() -> &'static [Move] {
+    pub fn get_bishop_moves() -> &'static [Self] {
         &Self::QUEEN_MOVES[4..8]
     }
 
-    pub fn get_knight_moves() -> &'static [Move] {
+    pub fn get_knight_moves() -> &'static [Self] {
         &Self::KNIGHT_MOVES
     }
 }
