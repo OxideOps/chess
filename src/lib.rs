5a2a368ec3dca8ac7ab9d90127474e27a0bc17cc
Daniel Albl <daniel.albl@vivint.com>
2023-06-17 18:53:10 -0600

    Give the widget its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // Expose modules to entire crate (so we can use public modules in crate/tests (integration tests))
 pub mod board;
+pub mod chess_widget;
 mod game;
 mod moves;
-pub mod pieces;
+mod pieces;
