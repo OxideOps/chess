e23ce4341beaee13796c833e1fac90b9e85b75d0
Daniel Albl <daniel.albl@vivint.com>
2023-06-20 10:45:46 -0600

    Update version

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use chess::chess_widget::{ChessWidget, WINDOW_SIZE};
 use druid::{AppLauncher, LocalizedString, WindowDesc};
 
 pub fn main() {
-    let window = WindowDesc::new(|| ChessWidget::new())
+    let window = WindowDesc::new(ChessWidget::new())
         .title(LocalizedString::new("Chess"))
         .window_size((WINDOW_SIZE, WINDOW_SIZE));
     AppLauncher::with_window(window)
-        .use_simple_logger()
         .launch("Druid + Piet".to_string())
         .expect("launch failed");
 }
