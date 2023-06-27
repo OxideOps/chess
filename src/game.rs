9a69e31d7507f91eed8379de33ed4016c756b2d2
Dillon-Roller <dillonroller.18@gmail.com>
2023-06-27 17:22:25 -0400

    `replay`

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,12 @@ use crate::board::Board;
 use crate::moves::Move;
 use crate::pieces::{Piece, Position};
 
+/// A result type for chess operations. 
+/// It's either an `Ok(())`, indicating the operation succeeded,
+/// or an `Err(ChessError)` indicating the operation failed with a specific error.
 pub type ChessResult = Result<(), ChessError>;
+
+/// An enumeration of possible errors that can occur during a game of chess.
 #[derive(Debug)]
 pub enum ChessError {
     OutOfBounds,           // Position is out of board bounds
@@ -17,6 +22,8 @@ pub enum ChessError {
     EmptyPieceMove,        // Trying to move an empty piece
 }
 
+/// A struct representing the current status of a chess game.
+/// The game can be either Ongoing, in Stalemate, Check, or Checkmate.
 #[derive(Default)]
 pub enum GameStatus {
     #[default]
@@ -26,25 +33,64 @@ pub enum GameStatus {
     Checkmate,
 }
 
+/// A struct representing a game of chess.
+/// It includes a board, a game status, a move history and a current move index.
 #[derive(Default)]
 pub struct Game {
+    /// The chessboard for the game.
     board: Board,
+    /// The status of the game.
     status: GameStatus,
+    /// The history of all moves made during the game.
+    move_history: Vec<Move>,
+    /// The current move index in the move history.
+    current_move: usize,
 }
 
 impl Game {
+    /// Gets the piece at the given position on the board. 
+    /// Returns `None` if there is no piece at the position.
     pub fn get_piece(&self, position: &Position) -> Option<Piece> {
         self.board.get_piece(position)
     }
 
+    /// Attempts to move a piece from one position to another on the board. 
+    /// Returns a `ChessResult`, indicating whether the move was successful.
     pub fn move_piece(&mut self, from: Position, to: Position) -> ChessResult {
         if let Some(piece) = self.board.get_piece(&from) {
             let mv = Move { from, to };
 
             self.board.move_piece(&mv)?;
+            self.move_history.push(mv);
+            self.current_move += 1;
             println!("{} : {}", piece, mv);
 
             self.board.next_turn();
+        }
+        Ok(())
+    }
+
+    /// Attempts to undo the last move.
+    /// Returns a `ChessResult`, indicating whether the undo was successful.
+    pub fn undo(&mut self) -> ChessResult {
+        self.board.move_piece(&self.move_history[self.current_move].inverse())?;
+        self.current_move -= 1;
+        Ok(())
+    }
+
+    /// Attempts to redo the last move.
+    /// Returns a `ChessResult`, indicating whether the redo was successful.
+    pub fn redo(&mut self) -> ChessResult {
+        self.board.move_piece(&self.move_history[self.current_move])?;
+        self.current_move += 1;
+        Ok(())
+    }
+
+    /// Attempts to resume the game after it was paused.
+    /// Returns a `ChessResult`, indicating whether the game could be resumed.
+    pub fn resume(&mut self) -> ChessResult {
+        while self.current_move < self.move_history.len() {
+            self.redo()?;
         }
         Ok(())
     }
--- src/moves.rs
+++ src/moves.rs
@@ -1,13 +1,45 @@
 use crate::pieces::Position;
 use std::fmt;
 
+/// A struct representing a move in a game. A move consists of a starting
+/// position (`from`) and an ending position (`to`).
+/// 
+/// This struct implements the `Copy`, `Clone`, `Debug`, `PartialEq`, `Eq`, and `Hash` traits.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Move {
+    /// The starting position of the move.
     pub from: Position,
+    /// The ending position of the move.
     pub to: Position,
 }
 
+impl Move {
+    /// Returns a new `Move` which is the inverse of the current one. That is, 
+    /// the `from` and `to` fields of the new `Move` are swapped compared to 
+    /// the current one.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use chess::{moves::Move, pieces::Position};
+    /// 
+    /// let move_1 = Move { from: Position { x: 1, y: 2 }, to: Position { x: 3, y: 4 } };
+    /// let move_2 = move_1.inverse();
+    /// assert_eq!(move_1.from, move_2.to);
+    /// assert_eq!(move_1.to, move_2.from);
+    /// ```
+    pub fn inverse(&self) -> Self {
+         Self {
+            from: self.to,
+            to: self.from,
+         }
+    }
+}
+
 impl fmt::Display for Move {
+    /// Formats the `Move` for display. The starting and ending positions are displayed
+    /// as coordinates on an 8x8 grid, where the x-coordinates are letters from 'a' to 'h' 
+    /// and the y-coordinates are numbers from '1' to '8'.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
         let ranks = ['1', '2', '3', '4', '5', '6', '7', '8'];
