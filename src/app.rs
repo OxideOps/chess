531237a01d39add3b2226c88dc29e7083a29fc6c
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2023-07-01 01:19:01 -0600

    Get empty board working in dioxus (#71) (#72)
    * Get empty board working in dioxus (#71)
    
    * Render pieces
    
    * Fix dependency
    
    * Potential fix for window size issue

---
+++ src/app.rs
@@ -0,0 +1,19 @@
+use crate::chess_widget::ChessWidget;
+use crate::game::Game;
+use dioxus::prelude::*;
+use dioxus_desktop::{use_window, LogicalSize};
+
+const WINDOW_SIZE: u32 = 800;
+
+pub fn App(cx: Scope) -> Element {
+    use_window(cx).set_inner_size(LogicalSize {
+        width: WINDOW_SIZE,
+        height: WINDOW_SIZE,
+    });
+    render! {
+        ChessWidget {
+            size: WINDOW_SIZE,
+            game: Game::default(),
+        },
+    }
+}
