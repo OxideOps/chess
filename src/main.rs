d659d9f8a42d5db2fcd3928812316bddda5e399f
Dillon Roller <dillonroller.18@gmail.com>
2023-07-03 03:35:21 -0400

    Build with trunk for github pages (#89)
    Co-authored-by: Daniel Albl <daniel.albl@vivint.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use chess::app::App;
 
 pub fn main() {
+    #[cfg(target_arch = "wasm32")]
+    dioxus_web::launch(App);
+    #[cfg(not(target_arch = "wasm32"))]
     dioxus_desktop::launch(App);
 }
