134aa907a5103f0c52e7c97cb60bcb9a38a2e99a
Daniel Albl <daniel.albl@vivint.com>
2023-07-03 00:17:29 -0600

    Build with trunk for github pages

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,20 @@
 use crate::chess_widget::ChessWidget;
 use dioxus::prelude::*;
+#[cfg(not(target_arch = "wasm32"))]
 use dioxus_desktop::{use_window, LogicalSize};
 
+#[cfg(not(target_arch = "wasm32"))]
 const WINDOW_SIZE: u32 = 800;
 
 pub fn App(cx: Scope) -> Element {
-    use_window(cx).set_inner_size(LogicalSize {
-        width: WINDOW_SIZE,
-        height: WINDOW_SIZE,
-    });
-    use_window(cx).set_title("Chess");
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        use_window(cx).set_inner_size(LogicalSize {
+            width: WINDOW_SIZE,
+            height: WINDOW_SIZE,
+        });
+        use_window(cx).set_title("Chess");
+    }
     render! {
         ChessWidget {},
     }
