5b8579afe90a67a80e2316a7b3cadc4d763fe75b
Daniel Albl <daniel.albl@vivint.com>
2023-07-03 16:29:38 -0600

    Set window config when created

--- src/app.rs
+++ src/app.rs
@@ -2,18 +2,6 @@ use crate::chess_widget::ChessWidget;
 use dioxus::prelude::*;
 
 pub fn App(cx: Scope) -> Element {
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        use dioxus_desktop::{use_window, LogicalSize};
-
-        const WINDOW_SIZE: u32 = 800;
-
-        use_window(cx).set_inner_size(LogicalSize {
-            width: WINDOW_SIZE,
-            height: WINDOW_SIZE,
-        });
-        use_window(cx).set_title("Chess");
-    }
     render! {
         ChessWidget {},
     }
