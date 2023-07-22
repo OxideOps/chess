a8e55da8692c8489f2cc0e9dc66983741ed9c4eb
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2023-07-22 16:07:38 -0600

    Use use_ref for game (#125)

--- client/src/web/game_socket.rs
+++ client/src/web/game_socket.rs
@@ -3,13 +3,10 @@ use chess::game::Game;
 use chess::moves::Move;
 
 use dioxus::prelude::*;
-use once_cell::sync::Lazy;
-use std::sync::RwLock;
 
-pub fn create_game_socket<'a>(
-    _cx: &'a Scoped<'a, ChessWidgetProps>,
-    _board_state_hash: &UseState<u64>,
-    _game: &'static Lazy<RwLock<Game>>,
-) -> Option<&'a Coroutine<Move>> {
+pub fn create_game_socket(
+    cx: Scope<ChessWidgetProps>,
+    game: &UseRef<Game>,
+) -> Option<Coroutine<Move>> {
     None
 }
