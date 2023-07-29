2524f49a529c1bedb84e9f80774b1571615c70bf
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2023-07-28 22:10:05 -0600

    Socket error handling (#155)
    * Log errors without panicing on the server
    
    * Add error handling to client web socket
    
    * Avoid this option nonsense with game socket streams
    
    * Make function for closing socket on server
    
    * Cleanup

--- client/src/game_socket.rs
+++ client/src/game_socket.rs
@@ -1,70 +1,59 @@
+use anyhow;
 use chess::game::Game;
 use chess::moves::Move;
 use dioxus::prelude::*;
-use futures::executor;
 use futures_util::{
+    join,
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
 };
-use std::sync::RwLock;
-use tokio_tungstenite_wasm::{connect, Message, WebSocketStream};
+use tokio_tungstenite_wasm::{connect, Message, Message::Text, Result, WebSocketStream};
 use url::Url;
-
-use crate::widget::WidgetProps;
 
 type WriteStream = SplitSink<WebSocketStream, Message>;
 type ReadStream = SplitStream<WebSocketStream>;
 
 const GAME_ID: u32 = 1234;
-static SOCKET_CREATED: RwLock<bool> = RwLock::new(false);
 
-fn init_streams() -> (Option<WriteStream>, Option<ReadStream>) {
-    if !*SOCKET_CREATED.read().unwrap() {
-        let (write, read) = executor::block_on(connect(
-            Url::parse(&format!("ws://muddy-fog-684.fly.dev/game/{GAME_ID}")).unwrap(),
-        ))
-        .unwrap()
-        .split();
-        *SOCKET_CREATED.write().unwrap() = true;
-        (Some(write), Some(read))
-    } else {
-        (None, None)
-    }
+pub async fn create_game_socket(game: UseRef<Game>, rx: UnboundedReceiver<Move>) {
+    match connect_to_socket().await {
+        Ok((write, read)) => {
+            join!(read_from_socket(read, &game), write_to_socket(rx, write));
+        }
+        Err(err) => log::error!("Error connecting game socket: {err:?}"),
+    };
 }
 
-async fn write_to_socket(mut rx: UnboundedReceiver<Move>, write_stream: Option<WriteStream>) {
-    if let Some(mut socket) = write_stream {
-        while let Some(mv) = rx.next().await {
-            log::info!("Sending move {mv:?}");
-            socket
-                .send(Message::Text(serde_json::to_string(&mv).unwrap()))
-                .await
-                .unwrap();
+async fn connect_to_socket() -> anyhow::Result<(WriteStream, ReadStream)> {
+    let url = format!("ws://muddy-fog-684.fly.dev/game/{GAME_ID}");
+    Ok(connect(Url::parse(&url)?).await?.split())
+}
+
+async fn send_move(mv: &Move, socket: &mut WriteStream) -> anyhow::Result<()> {
+    log::info!("Sending move {mv:?}");
+    socket.send(Text(serde_json::to_string(mv)?)).await?;
+    Ok(())
+}
+
+async fn write_to_socket(mut rx: UnboundedReceiver<Move>, mut socket: WriteStream) {
+    while let Some(mv) = rx.next().await {
+        if let Err(err) = send_move(&mv, &mut socket).await {
+            log::error!("Error sending move: {err:?}");
         }
     }
 }
 
-async fn read_from_socket(read_stream: Option<ReadStream>, game: UseRef<Game>) {
-    if let Some(mut stream) = read_stream {
-        while let Some(message) = stream.next().await {
-            let data = message.unwrap().into_text().unwrap();
-            let mv: Move =
-                serde_json::from_str(&data).expect("Failed to read move from remote player.");
-            log::info!("Got move {mv:?}");
-            game.with_mut(|game| game.move_piece(mv.from, mv.to).ok());
-        }
-    }
+fn handle_message(message: Result<Message>, game: &UseRef<Game>) -> anyhow::Result<()> {
+    let mv = serde_json::from_str::<Move>(&message?.into_text()?)?;
+    log::info!("Got move {mv:?}");
+    game.with_mut(|game| game.move_piece(mv.from, mv.to))?;
+    Ok(())
 }
 
-pub fn create_game_socket<'a>(
-    cx: Scope<'a, WidgetProps>,
-    game: &UseRef<Game>,
-) -> &'a Coroutine<Move> {
-    let (write_stream, read_stream) = init_streams();
-    use_coroutine(cx, |_rx: UnboundedReceiver<()>| {
-        read_from_socket(read_stream, game.to_owned())
-    });
-    use_coroutine(cx, |rx: UnboundedReceiver<Move>| {
-        write_to_socket(rx, write_stream)
-    })
+async fn read_from_socket(mut stream: ReadStream, game: &UseRef<Game>) {
+    while let Some(message) = stream.next().await {
+        if let Err(err) = handle_message(message, &game) {
+            log::error!("Error receiving move: {err:?}");
+        }
+    }
 }
