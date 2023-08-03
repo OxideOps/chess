60aae6b20c60fee9baf416e6dbd1660eed0d9d10
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2023-08-03 13:24:59 -0600

    Fix more socket issues (#183)
    * Use tls for web sockets on desktop
    
    * Add patch for local development
    
    * Use hash map of sockets
    
    * Cargo fmt

--- server-functions/src/setup_remote_game.rs
+++ server-functions/src/setup_remote_game.rs
@@ -10,23 +10,28 @@ pub struct RemoteGameInfo {
 
 #[cfg(feature = "ssr")]
 pub mod games {
+    use axum::extract::ws::{Message, WebSocket};
+    use futures::stream::{SplitSink, SplitStream};
     use once_cell::sync::Lazy;
-    use std::collections::HashSet;
-    use std::sync::Arc;
-    use tokio::sync::RwLock;
+    use std::{collections::HashMap, sync::Arc};
+    use tokio::sync::{Mutex, RwLock};
 
-    pub static GAMES: Lazy<Arc<RwLock<HashSet<u32>>>> =
-        Lazy::new(|| Arc::new(RwLock::new(HashSet::new())));
+    pub type WriteStream = Arc<Mutex<Option<SplitSink<WebSocket, Message>>>>;
+    pub type ReadStream = Arc<Mutex<Option<SplitStream<WebSocket>>>>;
+    pub type PlayerConnections = Arc<[(WriteStream, ReadStream); 2]>;
+
+    pub static GAMES: Lazy<Arc<Mutex<HashMap<u32, PlayerConnections>>>> =
+        Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
     pub static PENDING_GAME: Lazy<Arc<RwLock<Option<u32>>>> =
         Lazy::new(|| Arc::new(RwLock::new(None)));
 }
 
 #[server(SetupRemoteGame, "/api")]
 pub async fn setup_remote_game() -> Result<RemoteGameInfo, ServerFnError> {
-    use games::{GAMES, PENDING_GAME};
+    use games::{PlayerConnections, GAMES, PENDING_GAME};
     use rand::distributions::{Distribution, Uniform};
 
-    let mut games = GAMES.write().await;
+    let mut games = GAMES.lock().await;
     let mut pending_game = PENDING_GAME.write().await;
     if let Some(game_id) = *pending_game {
         *pending_game = None;
@@ -39,11 +44,11 @@ pub async fn setup_remote_game() -> Result<RemoteGameInfo, ServerFnError> {
     let mut rng = rand::thread_rng();
     let range = Uniform::from(1..10000000);
     let mut game_id = 0;
-    while games.contains(&game_id) {
+    while games.contains_key(&game_id) {
         game_id = range.sample(&mut rng);
     }
 
-    games.insert(game_id);
+    games.insert(game_id, PlayerConnections::default());
     *pending_game = Some(game_id);
 
     Ok(RemoteGameInfo {
