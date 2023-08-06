b80697be2fba09de9e3e7026b0f224984c206e6a
Daniel Albl <daniel.albl@vivint.com>
2023-08-05 18:02:12 -0600

    Allow flipping the board

--- client/src/app.rs
+++ client/src/app.rs
@@ -7,12 +7,23 @@ use chess::{
 };
 use dioxus::prelude::*;
 use server_functions::setup_remote_game::setup_remote_game;
-
 const WIDGET_HEIGHT: u32 = 800;
+const BUTTON_WIDTH: u32 = 100;
+
+fn get_default_perspective(white_player: &UseRef<Player>, black_player: &UseRef<Player>) -> Color {
+    if black_player.with(|player| player.kind == PlayerKind::Local)
+        && white_player.with(|player| player.kind != PlayerKind::Local)
+    {
+        Color::Black
+    } else {
+        Color::White
+    }
+}
 
 pub fn App(cx: Scope) -> Element {
     let white_player = use_ref(cx, || Player::with_color(Color::White));
     let black_player = use_ref(cx, || Player::with_color(Color::Black));
+    let perspective = use_state(cx, || Color::White);
     let game_id = use_state::<Option<u32>>(cx, || None);
 
     cx.render(rsx! {
@@ -22,6 +33,7 @@ pub fn App(cx: Scope) -> Element {
             game_id: *game_id.get(),
             white_player: white_player.to_owned(),
             black_player: black_player.to_owned(),
+            perspective: *perspective.get(),
             time: Duration::from_secs(3600),
             height: WIDGET_HEIGHT,
         },
@@ -29,6 +41,7 @@ pub fn App(cx: Scope) -> Element {
             onclick: |_| {
                 let white_player = white_player.to_owned();
                 let black_player = black_player.to_owned();
+                let perspective = perspective.to_owned();
                 let game_id = game_id.to_owned();
                 cx.spawn(async move {
                     match setup_remote_game().await {
@@ -40,13 +53,27 @@ pub fn App(cx: Scope) -> Element {
                                 Color::Black => white_player.to_owned(),
                             };
                             player.with_mut(|player| player.kind = PlayerKind::Remote);
+                            perspective.set(get_default_perspective(&white_player, &black_player));
                         },
                         Err(err) => log::error!("Error starting remote game: {err:?}"),
                     }
                 })
             },
-            style: "position: absolute; top: {WIDGET_HEIGHT}px",
+            class: "absolute",
+            style: "top: {WIDGET_HEIGHT}px; width: {BUTTON_WIDTH}px",
             "Play Remote",
+        },
+        button {
+            onclick: |_| {
+                let perspective = perspective.to_owned();
+                match perspective.get() {
+                    Color::White => perspective.set(Color::Black),
+                    Color::Black => perspective.set(Color::White),
+                }
+            },
+            class: "absolute",
+            style: "top: {WIDGET_HEIGHT}px; width: {BUTTON_WIDTH}px; left: {BUTTON_WIDTH}px",
+            "Flip Board",
         }
     })
 }
