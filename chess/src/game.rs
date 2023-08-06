4e8cebad973900f36ea61296520b907faac804d7
Dillon-Roller <dillonroller.18@gmail.com>
2023-08-06 18:10:38 -0400

    `cargo fmt`

--- chess/src/game.rs
+++ chess/src/game.rs
@@ -133,7 +133,8 @@ impl Game {
             return;
         }
         if self.get_active_time().is_zero() {
-            self.status.update(GameStatus::Timeout(self.get_current_player()));
+            self.status
+                .update(GameStatus::Timeout(self.get_current_player()));
             return;
         }
         if self.check_for_draw() {
@@ -145,9 +146,11 @@ impl Game {
         if !king_is_under_attack && valid_moves_is_empty {
             self.status.update(GameStatus::Stalemate)
         } else if king_is_under_attack && valid_moves_is_empty {
-            self.status.update(GameStatus::Checkmate(self.get_current_player()))
+            self.status
+                .update(GameStatus::Checkmate(self.get_current_player()))
         } else if king_is_under_attack {
-            self.status.update(GameStatus::Check(self.get_current_player()))
+            self.status
+                .update(GameStatus::Check(self.get_current_player()))
         } else {
             self.status.update(GameStatus::Ongoing)
         }
