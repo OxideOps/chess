86bcca54994063ba15de6e59efcb56de149b5494
Daniel Albl <daniel.albl@vivint.com>
2023-08-20 17:13:17 -0600

    Cleanup build.rs

--- client/build.rs
+++ client/build.rs
@@ -3,11 +3,7 @@ use std::process::Command;
 const STOCKFISH_SCRIPT: &str = "./build-stockfish.sh";
 
 fn main() {
-    let status = Command::new(STOCKFISH_SCRIPT)
+    Command::new(STOCKFISH_SCRIPT)
         .status()
-        .expect("Failed to execute script.");
-
-    if !status.success() {
-        panic!("{STOCKFISH_SCRIPT} did not run successfully.");
-    }
+        .expect("Failed to build stockfish");
 }
