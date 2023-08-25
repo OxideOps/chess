4782dbd384471377868c463b515f3b402e43b0da
Dillon-Roller <dillonroller.18@gmail.com>
2023-08-25 13:27:43 -0400

    clean up code

--- client/build.rs
+++ client/build.rs
@@ -1,8 +1,6 @@
 use std::process::Command;
 
-const STOCKFISH_SCRIPT: &str = "./build-stockfish.sh";
-const TRUNK_SCRIPT: &str = "./build-trunk.sh";
-const TAILWIND_SCRIPT: &str = "./build-npx.sh";
+const SCRIPTS: [&str; 3] = ["./build-stockfish.sh", "./build-npx.sh", "./build-trunk.sh"];
 
 fn run_script(command: &str) -> bool {
     Command::new(command)
@@ -12,18 +10,9 @@ fn run_script(command: &str) -> bool {
 }
 
 fn main() {
-    // Execute the STOCKFISH_SCRIPT
-    if !run_script(STOCKFISH_SCRIPT) {
-        panic!("Failed to build stockfish");
-    }
-
-    // Execute the TAILWIND_CSS command
-    if !run_script(TAILWIND_SCRIPT) {
-        panic!("Failed to run tailwind css command");
-    }
-
-    // Execute the TRUNK_BUILD command
-    if !run_script(TRUNK_SCRIPT) {
-        panic!("Failed to run trunk build");
+    for script in SCRIPTS.iter() {
+        if !run_script(script) {
+            panic!("Failed to run script: '{}'", script);
+        }
     }
 }
