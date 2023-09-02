474dcc443d97ae699ba0a38ac9690b8f293ba7e8
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-01 23:50:56 -0400

    updates

--- server/build.rs
+++ server/build.rs
@@ -6,12 +6,20 @@ fn main() {
     println!("cargo:rerun-if-changed=../client/Stockfish");
 
     let trunk_path = fs::canonicalize("..").unwrap();
+    let tailwind_program = fs::canonicalize("../client/build-tailwind.sh").unwrap();
 
-    let commands = vec![CommandConfig {
-        program: Path::new("trunk"),
-        args: Some(&["build"]),
-        dir: Some(&trunk_path),
-    }];
+    let commands = vec![
+        CommandConfig {
+            program: Path::new("trunk"),
+            args: Some(&["build"]),
+            dir: Some(&trunk_path),
+        },
+        CommandConfig {
+            program: &tailwind_program,
+            args: None,
+            dir: None,
+        }
+    ];
 
     CommandConfig::run_build_commands(&commands);
 }
