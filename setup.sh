51ab711703a70f109793dd1aee1332cd2a6ff91b
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-11 10:54:21 -0400

    remove redundant line in script

--- setup.sh
+++ setup.sh
@@ -36,7 +36,6 @@ setup_nodejs() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install node
 }
 
