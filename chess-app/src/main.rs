99b6cae100186f5c0a35243d9a3cd4be5e055ce1
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-18 02:11:44 -0400

    prototype code

--- chess-app/src/main.rs
+++ chess-app/src/main.rs
@@ -1,4 +1,13 @@
 use chess_app::*;
+use dioxus_fullstack::prelude::*;
 fn main() {
-    
+    let mut config = LaunchBuilder::new(App);
+
+    #[cfg(feature = "client")]
+    client::setup_config(&mut config);
+
+    #[cfg(feature = "server")]
+    server::setup_config(&mut config);
+
+    config.launch()
 }
