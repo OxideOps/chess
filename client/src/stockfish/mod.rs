3ec534a6da4fa02e14d75219fa9722a6db179451
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-19 22:36:58 -0400

    updates

--- client/src/stockfish/mod.rs
+++ client/src/stockfish/mod.rs
@@ -3,12 +3,10 @@ pub mod stockfish_interface;
 
 #[cfg(target_arch = "wasm32")]
 mod web;
-
 #[cfg(not(target_arch = "wasm32"))]
 mod desktop;
 
 #[cfg(target_arch = "wasm32")]
 pub use web::interface;
-
 #[cfg(not(target_arch = "wasm32"))]
 pub use desktop::interface;
