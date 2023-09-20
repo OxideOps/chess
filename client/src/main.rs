112c829186cd15a8cc03934258906c754c70d465
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-20 08:39:11 -0400

    updates

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 use client::components::App;
 use common::args::*;
-use dioxus_fullstack::prelude::LaunchBuilder;
+use dioxus_fullstack::prelude::*;
 
 pub fn main() {
     dioxus_logger::init(Args::parse().log_level).expect("Failed to initialize dioxus logger");
@@ -10,12 +10,12 @@ pub fn main() {
     #[cfg(target_arch = "wasm32")]
     {
         log::info!("web launching");
+        dioxus_web::launch(App);
     }
     #[cfg(not(target_arch = "wasm32"))]
     {
         use dioxus_desktop::{Config, WindowBuilder};
-        use dioxus_fullstack::prelude::server_fn::set_server_url;
-        set_server_url("https://oxide-chess.fly.dev");
+        server_fn::set_server_url("https://oxide-chess.fly.dev");
 
         log::info!("desktop launching");
 
