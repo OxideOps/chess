7c01c88a2a6886ecfea4a361f6f64fec279f6244
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-19 21:37:22 -0400

    `fmt`

--- client/src/stockfish/stockfish_interface.rs
+++ client/src/stockfish/stockfish_interface.rs
@@ -8,5 +8,9 @@ pub trait StockfishInterface {
     type Process;
     async fn send_command(&self, process: &UseRef<Option<Self::Process>>, command: &str);
     async fn run_stockfish(&self) -> Result<Self::Process, String>;
-    async fn update_analysis_arrows(&self, arrows: UseRef<Arrows>, process: UseRef<Option<Self::Process>>);
+    async fn update_analysis_arrows(
+        &self,
+        arrows: UseRef<Arrows>,
+        process: UseRef<Option<Self::Process>>,
+    );
 }
