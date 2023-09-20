7a2fbc96900ac154baa98caa0ebb1ceb95a5acff
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-20 00:55:48 -0400

    FIX EDITOR ERROR

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,24 @@
 use client::components::App;
 use common::args::*;
+use dioxus_fullstack::prelude::LaunchBuilder;
 
 pub fn main() {
     dioxus_logger::init(Args::parse().log_level).expect("Failed to initialize dioxus logger");
+
+    let config = LaunchBuilder::new(App);
+
     #[cfg(target_arch = "wasm32")]
     {
         log::info!("web launching");
-        dioxus_web::launch(App);
     }
     #[cfg(not(target_arch = "wasm32"))]
     {
         use dioxus_desktop::{Config, WindowBuilder};
         use dioxus_fullstack::prelude::server_fn::set_server_url;
         set_server_url("https://oxide-chess.fly.dev");
+
         log::info!("desktop launching");
+
         dioxus_desktop::launch_cfg(
             App,
             Config::new()
@@ -25,4 +30,5 @@ pub fn main() {
                 .with_disable_context_menu(true),
         );
     }
+
 }
