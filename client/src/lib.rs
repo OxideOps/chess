0a21980928dda6080518f8e795d7a2f3b253dea3
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-20 11:15:27 -0400

    updates

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,7 +2,7 @@
 mod arrows;
 pub mod components;
 mod game_socket;
-mod launcher;
+pub mod launcher;
 mod mouse_click;
 mod shared_states;
 mod stockfish;
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,31 +1,7 @@
-use client::components::App;
 use common::args::*;
 use dioxus_fullstack::prelude::*;
 
 pub fn main() {
     dioxus_logger::init(Args::parse().log_level).expect("Failed to initialize dioxus logger");
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        log::info!("web launching");
-        dioxus_web::launch(App);
-    }
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        use dioxus_desktop::{Config, WindowBuilder};
-        server_fn::set_server_url("https://oxide-chess.fly.dev");
-
-        log::info!("desktop launching");
-
-        dioxus_desktop::launch_cfg(
-            App,
-            Config::new()
-                .with_window(
-                    WindowBuilder::new()
-                        .with_title("Oxide Chess")
-                        .with_maximized(true),
-                )
-                .with_disable_context_menu(true),
-        );
-    }
+    client::launcher::launch();
 }
