6f7648efdc560e5ca54ff602e9d0cf9f9533e97e
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-20 18:07:52 -0400

    `cargo fmt`

--- client/src/launcher.rs
+++ client/src/launcher.rs
@@ -1,12 +1,12 @@
 pub fn launch() {
-    #[cfg(not(target_arch= "wasm32"))]
+    #[cfg(not(target_arch = "wasm32"))]
     {
-        use dioxus_fullstack::prelude::server_fn;
         use dioxus_desktop::{Config, WindowBuilder};
+        use dioxus_fullstack::prelude::server_fn;
 
         log::info!("configuring desktop..");
         server_fn::set_server_url("https://oxide-chess.fly.dev");
-    
+
         dioxus_desktop::launch_cfg(
             crate::components::App,
             Config::new()
