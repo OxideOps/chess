99cafcae8194fafceb66ea1c0c9e1137c4378f9d
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-20 09:28:57 -0400

    update

--- client/src/launcher.rs
+++ client/src/launcher.rs
@@ -1,17 +1,17 @@
 use dioxus_fullstack::prelude::LaunchBuilder;
 
 pub fn launch() {
-    let _builder = LaunchBuilder::new(crate::components::App);
+    let config = LaunchBuilder::new(crate::components::App);
 
 
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-fn launch_desktop<Props: Clone>(builder: &mut LaunchBuilder<Props>) {
+fn configure_desktop<Props: Clone>(builder: &mut LaunchBuilder<Props>) {
 
 }
 
 #[cfg(target_arch = "wasm32")]
-fn launch_web<Props: Clone>(builder: &mut LaunchBuilder<Props>) {
+fn configure_web<Props: Clone>(builder: &mut LaunchBuilder<Props>) {
 
 }
