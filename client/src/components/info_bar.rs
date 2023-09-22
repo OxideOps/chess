0d845f445cb65583a52e5395e086a8b6e11d80ea
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-22 18:47:34 -0400

    updates

--- client/src/components/info_bar.rs
+++ client/src/components/info_bar.rs
@@ -1,7 +1,6 @@
 use crate::components::RoundList;
 use crate::components::Timer;
 
-use chess::game::Game;
 use dioxus::prelude::*;
 use std::time::Duration;
 
@@ -12,10 +11,9 @@ pub struct InfoBarProps {
 }
 
 pub fn InfoBar(cx: Scope<InfoBarProps>) -> Element {
-    let game = use_shared_state::<Game>(cx).unwrap();
     cx.render(rsx! {
         div { class: "info-bar-container", style: "left: {cx.props.left}px;",
-            Timer { start_time: cx.props.start_time }
+            Timer { start_time: cx.props.start_time },
             RoundList {}
         }
     })
