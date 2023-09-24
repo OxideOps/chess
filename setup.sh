828c88fa6ea5dededd0064c7c170006dd14cbb2b
Daniel Albl <daniel.albl@vivint.com>
2023-09-23 22:16:54 -0600

    Add git to docker

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DOCKER_MODE=false
 
 # Make the server functions not change based on the path to the repo 
 ENV_VAR="SERVER_FN_OVERRIDE_KEY=y"
-PACKAGES="build-essential curl libjavascriptcoregtk-4.1-dev libgtk-3-dev libsoup-3.0-dev libssl-dev libwebkit2gtk-4.1-dev"
+PACKAGES="build-essential curl git libjavascriptcoregtk-4.1-dev libgtk-3-dev libsoup-3.0-dev libssl-dev libwebkit2gtk-4.1-dev"
 
 trap 'echo "Setup failed!"; exit 1' ERR
 
