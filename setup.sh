10f40276dc368b20913a0d7276812b18408226d6
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2023-09-23 21:51:44 -0600

    Bugfix (#324)
    * Bugfix
    
    * Have setup.sh fail if a command fails

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ DOCKER_MODE=false
 ENV_VAR="SERVER_FN_OVERRIDE_KEY=y"
 PACKAGES="build-essential curl libjavascriptcoregtk-4.1-dev libgtk-3-dev libsoup-3.0-dev libssl-dev libwebkit2gtk-4.1-dev"
 
+trap 'echo "Setup failed!"; exit 1' ERR
+
 terminate_script() {
     local message="$1"
     echo "$message" >&2
@@ -30,9 +32,9 @@ parse_arguments() {
 
 install_packages() {
     if "$DOCKER_MODE"; then
-        apt-get update && apt-get upgrade -y && apt-get install -y "$PACKAGES"
+        apt-get update && apt-get upgrade -y && apt-get install -y $PACKAGES
     else
-        sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y "$PACKAGES"
+        sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y $PACKAGES
     fi
 }
 
