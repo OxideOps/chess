5b5566cc8dcada8f1b983ea7ed5b46113ed23c45
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-25 16:30:19 -0400

    `cargo fmt`

--- chess/src/position.rs
+++ chess/src/position.rs
@@ -18,7 +18,7 @@ pub struct Position {
 impl Position {
     pub(super) const WHITE_KING: Position = Position::new(4, 0);
     pub(super) const BLACK_KING: Position = Position::new(4, 7);
-    
+
     pub const fn new(x: usize, y: usize) -> Self {
         Self { x, y }
     }
