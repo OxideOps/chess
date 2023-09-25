c4e548d683a0bb0d26f2cb2a04a620a0213bb169
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-25 17:15:01 -0400

    updates

--- client/src/stockfish/stockfish_interface.rs
+++ client/src/stockfish/stockfish_interface.rs
@@ -2,7 +2,8 @@ use dioxus::prelude::{UseAsyncLock, UseLock};
 
 pub trait Stockfish {
     type Process;
+    type Error;
     async fn send_command(&self, process: &UseAsyncLock<Option<Self::Process>>, command: &str);
-    async fn run_stockfish(&self) -> Result<Self::Process>;
+    async fn run_stockfish(&self) -> Result<Self::Process, Self::Error>;
     async fn update_analysis_arrows(&self, arrows: UseLock<Arrows>, process: UseAsyncLock<Option<Self::Process>>);
 }
