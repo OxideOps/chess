33644a188c6478d20d789e3952ad265808688f56
Dillon-Roller <dillonroller.18@gmail.com>
2023-10-01 16:55:45 -0400

    refactor

--- client/src/components/app.rs
+++ client/src/components/app.rs
@@ -86,19 +86,17 @@ pub(crate) fn App(cx: Scope) -> Element {
                 onclick: |_| analyze.modify(|analyze| !analyze),
                 if *analyze.get() { "Stop analyzing" } else { "Analyze" }
             }
+            #[cfg(not(target_arch = "wasm32"))]
             {
-                #[cfg(not(target_arch = "wasm32"))]
-                {
-                    rsx! {
-                        button {
-                            class: "button",
-                            style: "top: {WIDGET_HEIGHT}px",
-                            onclick: |_| {
-                                log::info!("Quitting game..");
-                                window.close()
-                            },
-                            "Quit"
-                        }
+                rsx! {
+                    button {
+                        class: "button",
+                        style: "top: {WIDGET_HEIGHT}px",
+                        onclick: |_| {
+                            log::info!("Quitting game..");
+                            window.close()
+                        },
+                        "Quit"
                     }
                 }
             }
