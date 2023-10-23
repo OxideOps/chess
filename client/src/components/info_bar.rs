6a8a54ec26602c13da4960b3c578e55fd52af28d
Dillon-Roller <dillonroller.18@gmail.com>
2023-10-22 23:47:19 -0400

    refactor `info_bar.rs`

--- client/src/components/info_bar.rs
+++ client/src/components/info_bar.rs
@@ -4,16 +4,11 @@ use crate::components::Timer;
 use dioxus::prelude::*;
 use std::time::Duration;
 
-#[derive(Props, PartialEq)]
-pub(crate) struct InfoBarProps {
-    start_time: Duration,
-    left: u32,
-}
-
-pub(crate) fn InfoBar(cx: Scope<InfoBarProps>) -> Element {
+#[component]
+pub(crate) fn InfoBar(cx: Scope, start_time: Duration, left: u32) -> Element {
     cx.render(rsx! {
-        div { class: "info-bar-container", style: "left: {cx.props.left}px;",
-            Timer { start_time: cx.props.start_time }
+        div { class: "info-bar-container", style: "left: {left}px;",
+            Timer { start_time: *start_time }
             RoundList {}
         }
     })
