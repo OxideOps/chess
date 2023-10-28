0fa787f478924774f698b999917dfcd4985cd1fc
Daniel Albl <daniel.albl@vivint.com>
2023-10-27 22:27:28 -0600

    Show eval text

--- client/src/components/eval_bar.rs
+++ client/src/components/eval_bar.rs
@@ -8,14 +8,20 @@ const EVAL_SENSITIVITY: f64 = 1.0 / 800.0;
 pub(crate) fn EvalBar(cx: Scope, perspective: Color) -> Element {
     let eval = **use_shared_state::<Eval>(cx).unwrap().read();
     let percent = 100.0 * sigmoid(EVAL_SENSITIVITY * eval);
-    let direction = match perspective {
-        Color::White => "top",
-        Color::Black => "bottom",
+    let (text_color, direction) = match perspective {
+        Color::White => ("black", "top"),
+        Color::Black => ("white", "bottom"),
     };
     cx.render(rsx! {
         div {
             class: "eval-container",
-            style: "background: linear-gradient(to {direction}, white 0%, white {percent}%, black {percent}%, black 100%);"
+            style: "
+                background: linear-gradient(
+                    to {direction}, white 0%, white {percent}%, black {percent}%, black 100%
+                );
+                color: {text_color};
+            ",
+            "{eval / 100.0:.1}"
         }
     })
 }
