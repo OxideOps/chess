00189e33f7ab341ba521972b7f821f5a30717d0e
Dillon-Roller <dillonroller.18@gmail.com>
2023-10-29 22:37:12 -0400

    implement server function

--- client/src/components/widget.rs
+++ client/src/components/widget.rs
@@ -5,8 +5,6 @@ use super::InfoBar;
 use chess::color::Color;
 use chess::player::Player;
 use dioxus::prelude::*;
-use std::fs;
-use std::io;
 use std::time::Duration;
 
 #[component]
@@ -66,31 +64,4 @@ pub(crate) fn Widget(
             }
         }
     })
-}
-
-enum ThemeType {
-    Board,
-    Piece,
-}
-
-fn get_themes(theme_type: ThemeType) -> io::Result<Vec<String>> {
-    let mut themes = Vec::new();
-    let dir_path = match theme_type {
-        ThemeType::Board => "images/boards/",
-        ThemeType::Piece => "images/pieces/",
-    };
-
-    for entry in fs::read_dir(dir_path)? {
-        let path = entry?.path();
-
-        if path.is_dir() {
-            if let Some(theme_name) = path.file_name() {
-                if let Some(theme_str) = theme_name.to_str() {
-                    themes.push(theme_str.to_string());
-                }
-            }
-        }
-    }
-
-    Ok(themes)
 }
--- server-functions/src/lib.rs
+++ server-functions/src/lib.rs
@@ -3,3 +3,36 @@ mod setup_remote_game;
 #[cfg(feature = "ssr")]
 pub use setup_remote_game::games::*;
 pub use setup_remote_game::*;
+
+use dioxus_fullstack::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ThemeType {
+    Board,
+    Piece,
+}
+
+#[server(GetThemes, "/api")]
+pub async fn get_themes(theme_type: ThemeType) -> Result<Vec<String>, ServerFnError> {
+    let mut themes = Vec::new();
+    let dir_path = match theme_type {
+        ThemeType::Board => "images/boards/",
+        ThemeType::Piece => "images/pieces/",
+    };
+
+    for entry in fs::read_dir(dir_path)? {
+        let path = entry?.path();
+
+        if path.is_dir() {
+            if let Some(theme_name) = path.file_name() {
+                if let Some(theme_str) = theme_name.to_str() {
+                    themes.push(theme_str.to_string());
+                }
+            }
+        }
+    }
+
+    Ok(themes)
+}
