e55ab2cce1496b10ea4b7cff744af65ae1a250d0
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-01 18:17:20 -0400

    fix path

--- common/src/theme.rs
+++ common/src/theme.rs
@@ -9,10 +9,13 @@ pub enum ThemeType {
 
 pub async fn get_themes(theme_type: ThemeType) -> io::Result<Vec<String>> {
     let mut themes = Vec::new();
-    let dir_path = match theme_type {
-        ThemeType::Board => "images/boards/",
-        ThemeType::Piece => "images/pieces/",
+    let mut dir_path = match theme_type {
+        ThemeType::Board => "images/boards/".to_string(),
+        ThemeType::Piece => "images/pieces/".to_string(),
     };
+    if cfg!(feature = "ssr") {
+        dir_path.insert_str(0, "dist/");
+    }
 
     for entry in fs::read_dir(dir_path)? {
         let path = entry?.path();
