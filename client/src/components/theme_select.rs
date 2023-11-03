3f360e3f48ed8c8b1e180dbad136ad95deb01986
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-03 18:27:07 -0400

    use a table

--- client/src/components/theme_select.rs
+++ client/src/components/theme_select.rs
@@ -11,25 +11,29 @@ pub(crate) fn ThemeSelect(
     let piece_theme_list = get_theme_future(cx, ThemeType::Piece);
 
     cx.render(rsx! {
-        div {
-            div {
-                label { "Board theme: " }
-                select {
-                    class: "select",
-                    onchange: |event| board_theme.set(event.value.clone()),
-                    board_theme_list.value().into_iter().flat_map(|themes| themes.iter()).map(|theme|
-                        rsx! { option { value: "{theme}", "{theme}" } }
-                    )
+        table {
+            tr {
+                td {
+                    label { "Board theme: " }
+                    select {
+                        class: "select",
+                        onchange: |event| board_theme.set(event.value.clone()),
+                        board_theme_list.value().into_iter().flat_map(|themes| themes.iter()).map(|theme|
+                            rsx! { option { value: "{theme}", "{theme}" } }
+                        )
+                    }
                 }
             }
-            div {
-                label { "Piece theme: " }
-                select {
-                    class: "select",
-                    onchange: |event| piece_theme.set(event.value.clone()),
-                    piece_theme_list.value().into_iter().flat_map(|themes| themes.iter()).map(|theme|
-                        rsx! { option { value: "{theme}", "{theme}" } }
-                    )
+            tr {
+                td {
+                    label { "Piece theme: " }
+                    select {
+                        class: "select",
+                        onchange: |event| piece_theme.set(event.value.clone()),
+                        piece_theme_list.value().into_iter().flat_map(|themes| themes.iter()).map(|theme|
+                            rsx! { option { value: "{theme}", "{theme}" } }
+                        )
+                    }
                 }
             }
         }
