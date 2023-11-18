efc722dbcb600e8897b7da2a401f1cd75200c85f
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-18 17:04:32 -0500

    `dx build --features web` works

--- client/src/components/widget.rs
+++ client/src/components/widget.rs
@@ -3,8 +3,10 @@ use common::theme::ThemeType;
 use dioxus::prelude::*;
 
 use super::{
-    settings::{load_theme_from_config, Settings},
-    Board, EvalBar, InfoBar,
+    // settings::{load_theme_from_config, Settings},
+    Board,
+    EvalBar,
+    InfoBar,
 };
 use crate::shared_states::BoardSize;
 
@@ -15,8 +17,8 @@ pub(crate) fn Widget(
     black_player: UseLock<Player>,
     analyze: UseState<bool>,
 ) -> Element {
-    let board_theme = use_state(cx, || load_theme_from_config(ThemeType::Board));
-    let piece_theme = use_state(cx, || load_theme_from_config(ThemeType::Piece));
+    // let board_theme = use_state(cx, || load_theme_from_config(ThemeType::Board));
+    // let piece_theme = use_state(cx, || load_theme_from_config(ThemeType::Piece));
     let board_size = **use_shared_state::<BoardSize>(cx)?.read();
 
     cx.render(rsx! {
@@ -25,17 +27,17 @@ pub(crate) fn Widget(
                 white_player_kind: white_player.read().kind,
                 black_player_kind: black_player.read().kind,
                 analyze: analyze.to_owned(),
-                board_theme: board_theme.to_string(),
-                piece_theme: piece_theme.to_string(),
+                board_theme: Default::default(),
+                piece_theme: Default::default(),
             },
             if **analyze {
                 rsx! { EvalBar {} }
             },
             InfoBar {},
-            Settings {
-                board_theme: board_theme.to_owned(),
-                piece_theme: piece_theme.to_owned(),
-            },
+            // Settings {
+            //     board_theme: board_theme.to_owned(),
+            //     piece_theme: piece_theme.to_owned(),
+            // },
         }
     })
 }
