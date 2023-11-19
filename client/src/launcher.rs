9bdcf3cb4c49026f6522d9731801d83dd1787ca9
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-19 15:40:55 -0500

    updates

--- client/src/launcher.rs
+++ client/src/launcher.rs
@@ -2,6 +2,8 @@ pub fn launch() {
     #[cfg(feature = "desktop")]
     {
         use dioxus_desktop as desktop;
+        use dioxus_fullstack::prelude::server_fn;
+
         server_fn::set_server_url("https://oxide-chess.fly.dev/");
         log::info!("configuring desktop..");
         desktop::launch_cfg(
@@ -26,13 +28,16 @@ pub fn launch() {
             routing::get,
             ServiceExt,
         };
-        use common::args::*;
         use dioxus_fullstack::prelude::*;
         use tower::ServiceExt as OtherServiceExt;
         use tower_http::services::ServeDir;
         tokio::runtime::Runtime::new()
             .unwrap()
             .block_on(async move {
+                if dotenvy::dotenv().is_err() {
+                    log::warn!(".env file not found, continuing without loading")
+                }
+
                 log::info!("server launching");
                 let addr = "[::]:8080".parse().unwrap();
                 log::info!("listening on {}", addr);
@@ -41,6 +46,13 @@ pub fn launch() {
                         axum::Router::new()
                             .nest_service("/", ServeDir::new("dist"))
                             .nest_service("/images", ServeDir::new("images"))
+                            .register_server_fns("/api")
+                            .route(
+                                "/game/:game_id",
+                                get(move |Path::<u32>(game_id), ws: WebSocketUpgrade| {
+                                    handler(game_id, ws)
+                                }),
+                            )
                             .map_response(|mut response| {
                                 response.headers_mut().insert(
                                     "Cross-Origin-Opener-Policy",
