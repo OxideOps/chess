b0b14341f5fcfe894785e98e77bd825a3a0f4ea2
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-22 02:40:29 -0500

    cargo fmt

--- client/src/server/server_functions/mod.rs
+++ client/src/server/server_functions/mod.rs
@@ -3,5 +3,5 @@ pub mod games;
 mod get_themes;
 mod setup_remote_game;
 
-pub(crate) use setup_remote_game::setup_remote_game;
 pub(crate) use get_themes::get_themes;
+pub(crate) use setup_remote_game::setup_remote_game;
