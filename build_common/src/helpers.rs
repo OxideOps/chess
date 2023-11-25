2e3506d888b9c68f96eb66b51b6b23282197d87c
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-25 02:59:35 -0500

    refactor

--- build_common/src/helpers.rs
+++ build_common/src/helpers.rs
@@ -66,9 +66,9 @@ pub fn get_trunk_commands() -> Vec<CommandConfig> {
         "{}/wasm",
         env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".into())
     );
-    let mut args = vec!["build".into()];
-    args.push("--features".into());
-    args.push("web".into());
+
+    let mut args = vec!["build".into(), "--features".into(), "web".into()];
+
     if env::var("PROFILE") == Ok("release".into()) {
         args.push("--release".into());
     }
