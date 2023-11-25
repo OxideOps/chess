65c71feb815211fb06f897531d1b7972f9d41956
Dillon-Roller <dillonroller.18@gmail.com>
2023-11-25 17:37:28 -0500

    sea-orm database

--- app/src/server/mod.rs
+++ app/src/server/mod.rs
@@ -1,7 +1,9 @@
+mod database;
 #[cfg(feature = "ssr")]
 mod game_socket;
 #[cfg(feature = "ssr")]
 mod launcher;
 pub(crate) mod server_functions;
+
 #[cfg(feature = "ssr")]
 pub use launcher::launch;
