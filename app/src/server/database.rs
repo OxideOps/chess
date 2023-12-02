770e239122eaacdf04410a8f9f3de8451a05bc15
Dillon-Roller <dillonroller.18@gmail.com>
2023-12-01 22:00:14 -0500

    separate concerns for database, qualify name for handler to help make clearer

--- app/src/server/database.rs
+++ app/src/server/database.rs
@@ -6,12 +6,13 @@ use sqlx::{migrate::Migrator, pool::PoolOptions, Error, Pool, Postgres};
 static MIGRATOR: Migrator = sqlx::migrate!();
 static POOL: OnceCell<Pool<Postgres>> = OnceCell::new();
 
-pub async fn run_migrations() -> Result<(), Error> {
+async fn run_migrations() -> Result<(), Error> {
     MIGRATOR.run(&*POOL.get().unwrap()).await?;
+
     Ok(())
 }
 
-pub async fn init_db_pool() -> Result<(), Error> {
+async fn init_db_pool() -> Result<(), Error> {
     POOL.set(
         PoolOptions::<Postgres>::new()
             .max_connections(5)
@@ -20,5 +21,13 @@ pub async fn init_db_pool() -> Result<(), Error> {
             .unwrap(),
     )
     .expect("database pool must be empty on initialization");
+
+    Ok(())
+}
+
+pub async fn connect() -> Result<(), Error> {
+    init_db_pool().await?;
+    run_migrations().await?;
+
     Ok(())
 }
