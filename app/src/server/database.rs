d7b9ad0c19f135bc7b1fe2728a99e39ba76b1430
Dillon Roller <dillonroller.18@gmail.com>
2023-12-02 14:16:43 -0500

    server refactor (#483)
    * separate concerns for database, qualify name for handler to help make clearer
    
    * clean up

--- app/src/server/database.rs
+++ app/src/server/database.rs
@@ -6,19 +6,23 @@ use sqlx::{migrate::Migrator, pool::PoolOptions, Error, Pool, Postgres};
 static MIGRATOR: Migrator = sqlx::migrate!();
 static POOL: OnceCell<Pool<Postgres>> = OnceCell::new();
 
-pub async fn run_migrations() -> Result<(), Error> {
-    MIGRATOR.run(&*POOL.get().unwrap()).await?;
-    Ok(())
+async fn run_migrations() -> Result<(), Error> {
+    Ok(MIGRATOR.run(POOL.get().unwrap()).await?)
 }
 
-pub async fn init_db_pool() -> Result<(), Error> {
+async fn init_db_pool() -> Result<(), Error> {
     POOL.set(
         PoolOptions::<Postgres>::new()
             .max_connections(5)
             .connect(&env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-            .await
-            .unwrap(),
+            .await?,
     )
     .expect("database pool must be empty on initialization");
+
     Ok(())
+}
+
+pub async fn connect() -> Result<(), Error> {
+    init_db_pool().await?;
+    run_migrations().await
 }
--- app/src/server/launcher.rs
+++ app/src/server/launcher.rs
@@ -7,7 +7,7 @@ use dioxus_fullstack::prelude::*;
 use tower::ServiceExt as OtherServiceExt;
 use tower_http::services::ServeDir;
 
-use super::{database, game_socket::handler};
+use super::{database, game_socket};
 
 pub fn launch() {
     if dotenvy::dotenv().is_err() {
@@ -25,8 +25,7 @@ async fn setup_and_serve() {
     const ADDR: &str = "[::]:8080";
 
     log::info!("connecting to database");
-    database::init_db_pool().await.unwrap();
-    database::run_migrations().await.unwrap();
+    database::connect().await.unwrap();
 
     log::info!("listening on {}", ADDR);
     axum::Server::bind(&ADDR.parse().unwrap())
@@ -37,7 +36,9 @@ async fn setup_and_serve() {
                 .register_server_fns("/api")
                 .route(
                     "/game/:game_id",
-                    get(move |Path::<u32>(game_id), ws: WebSocketUpgrade| handler(game_id, ws)),
+                    get(move |Path::<u32>(game_id), ws: WebSocketUpgrade| {
+                        game_socket::handler(game_id, ws)
+                    }),
                 )
                 .map_response(|mut response| {
                     response
