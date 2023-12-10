f8fa8a316bb6381fcb43a719c46f09df77fd14c9
daniel.albl <daniel.albl@vivint.com>
2023-12-10 15:46:32 -0700

    Fix border messing with labels

--- app/src/client/components/board_square.rs
+++ app/src/client/components/board_square.rs
@@ -11,7 +11,11 @@ pub(crate) fn BoardSquare(cx: Scope, class: String, position: Position, hovered:
     let board_size = **use_shared_state::<BoardSize>(cx)?.read();
     let perspective = **use_shared_state::<Perspective>(cx)?.read();
     let top_left = to_point(board_size, perspective, position);
-    let border_class = if *hovered { "border-hover-square" } else { "" };
+    let border_class = if *hovered && class != "text-square" {
+        "border-hover-square"
+    } else {
+        ""
+    };
     cx.render(rsx! {
         div {
             class: "board-square {class} {border_class}",
