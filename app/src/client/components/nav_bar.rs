6992ca309a8a5aa33ba3156af9357b1195da0e62
Dillon-Roller <dillonroller.18@gmail.com>
2023-12-25 20:41:00 -0500

    start with settings

--- app/src/client/components/nav_bar.rs
+++ app/src/client/components/nav_bar.rs
@@ -14,56 +14,14 @@ pub(crate) fn NavBar(cx: Scope) -> Element {
                     li {
                         Link {
                             class: "nav-link",
-                            to: Route::Home {},
-                            "Home"
-                        }
-                    }
-                    li {
-                        Link {
-                            class: "nav-link",
                             to: Route::Widget {},
                             "Game"
-                        }
-                    }
-                    li {
-                        Link {
-                            class: "nav-link",
-                            to: Route::Puzzles {},
-                            "Puzzles"
-                        }
-                    }
-                    li {
-                        Link {
-                            class: "nav-link",
-                            to: Route::About {},
-                            "About"
                         }
                     }
                 }
             }
         }
         Outlet::<Route> {}
-    }
-}
-
-#[component]
-pub(crate) fn Home(cx: Scope) -> Element {
-    render! {
-        h1 { "Home Page" }
-    }
-}
-
-#[component]
-pub(crate) fn About(cx: Scope) -> Element {
-    render! {
-        h1 { "About Page" }
-    }
-}
-
-#[component]
-pub(crate) fn Puzzles(cx: Scope) -> Element {
-    render! {
-        h1 { "Puzzles Page" }
     }
 }
 
--- app/src/client/router.rs
+++ app/src/client/router.rs
@@ -1,20 +1,16 @@
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 
-use super::components::{nav_bar::*, Widget};
+use super::components::{nav_bar::*, Widget, };
 
 #[derive(Routable, Clone)]
 #[rustfmt::skip]
 pub(crate) enum Route {
     #[layout(NavBar)]
         #[route("/")]
-        Home {},
-        #[route("/game")]
         Widget {},
-        #[route("/puzzles")]
-        Puzzles {},
-        #[route("/about")]
-        About {},
+        #[route("/settings")]
+        Settings {},
     #[end_layout]
     #[route("/:..route")]
     PageNotFound { route: Vec<String> },
