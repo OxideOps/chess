01a0a71d8ce717cf58af2d575ecffef9166274f7
Dillon-Roller <dillonroller.18@gmail.com>
2023-12-30 04:02:57 -0500

    down migration

--- database/migrations/20231230083231_email_verification_tokens.down.sql
+++ database/migrations/20231230083231_email_verification_tokens.down.sql
@@ -1,1 +1,5 @@
--- Add down migration script here
+DROP TABLE IF EXISTS email_verification_tokens;
+
+ALTER TABLE accounts DROP COLUMN IF EXISTS email_verified;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
