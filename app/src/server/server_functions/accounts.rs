9a59a193d9c14a9befa6cf3b6b3a168acfab8092
Dillon-Roller <dillonroller.18@gmail.com>
2024-01-03 09:55:52 -0500

    allow unused for now

--- app/src/server/server_functions/accounts.rs
+++ app/src/server/server_functions/accounts.rs
@@ -6,13 +6,13 @@ pub async fn create_account(
     password: String,
     email: String,
 ) -> Result<(), ServerFnError> {
-    use crate::server::{auth, mailer};
+    use crate::server::auth;
 
     let hashed_password = auth::hash_password(&password).unwrap();
 
     let account_id = database::create_account(&username, &hashed_password, &email).await?;
 
-    let token = database::create_email_verification_token(account_id).await?;
+    let _token = database::create_email_verification_token(account_id).await?;
 
     //mailer::send_verification_email(&email, &token).await;
     Ok(())
